72409fc337aa6bac10be46688ea665b273ca95a9
afroemmel <127488242+afroemmel@users.noreply.github.com>
2023-05-09 01:27:37 +0200

    Update strange_pattern.py

--- strange_pattern.py
+++ strange_pattern.py
@@ -2,13 +2,16 @@ import numpy as np
 
 # implement the function strange pattern
 
-def strange_pattern((n,m)):
-    pattern = np.zeros((n, m), dtype=bool)
+def strange_pattern(nm):
+    n, m = nm
+    pattern = np.empty((n, m), dtype=str)
     
     for i in range(n):
         for j in range(m):
             if (i % 3 == 0 or i % 3 == 2) and (j % 3 == 0 or j % 3 == 2):
-                pattern[i, j] = True
+                pattern[i, j] = 'x'
+            else:
+                pattern[i, j] = ' '
     
     return pattern
 
