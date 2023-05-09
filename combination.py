b4e6a2b80bc459c6ff2cc578553819a6e48d3017
arne <froemmel.arne@gmx.de>
2023-05-09 02:15:11 +0200

    test

--- combination.py
+++ combination.py
@@ -3,21 +3,17 @@ import numpy as np
 # implement your function to combine two numpy arrays 
 
 def combination(arr1, arr2, axis=0):
-    # Remove unnecessary dimensions of input arrays
-    arr1 = np.squeeze(arr1)
-    arr2 = np.squeeze(arr2)
-
-    # Check if arrays can be combined along the given axis
+    arr1, arr2 = np.squeeze(arr1), np.squeeze(arr2)
     if arr1.shape[:axis] != arr2.shape[:axis] or arr1.shape[axis + 1:] != arr2.shape[axis + 1:]:
-        raise ValueError("Cannot combine arrays along the specified axis.")
-
-    # Combine arrays along the given axis
-    combined = np.concatenate((arr1, arr2), axis=axis)
-
-    return combined
-
+        raise ValueError("meaningful error")
+    return np.concatenate((arr1, arr2), axis=axis)
 
 if __name__ == "__main__":
-    # use this for your own testing!
+    arr1 = np.array([["x","x","x"], ["y","y","y"]])
+    arr2 = np.array([["z","z","z"], ["a","a","a"]])
+    combined0 = combination(arr1, arr2, axis=0)
+    combined1 = combination(arr1, arr2, axis=1)
+    print(combined0)
+    print(combined1)
 
     pass
