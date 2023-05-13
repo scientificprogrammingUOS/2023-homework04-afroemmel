dd11ff83e923606b2007d616107301ee98382b66
arne <froemmel.arne@gmx.de>
2023-05-13 16:46:54 +0200

    test

--- gaussian_analysis.py
+++ gaussian_analysis.py
@@ -2,10 +2,23 @@ import numpy as np
 
 # implement the function gaussian_analysis
 
-def gaussian_analysis():
-        
-    # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
+def gaussian_analysis(loc, scale, low, high):
+    meaningfulError = "much meaningful so wow"
+    if type(loc) is not int and type(loc) is not float:
+        return meaningfulError
+    if type(scale) is not int and type(scale) is not float:
+        return meaningfulError
+    if type(low) is not int and type(low) is not float:
+        return meaningfulError
+    if type(high) is not int and type(high) is not float:
+        return meaningfulError
+    if low > high:
+        return meaningfulError
+
+    arr = np.random.normal(loc, scale, 100)
+    arr = arr[arr >= low]
+    arr = arr[arr <= high]
+    return np.mean(arr), np.std(arr)
 
 
 if __name__ == "__main__":
